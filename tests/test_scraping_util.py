044ccd7ddbf0743c55a9d65d938aa4b331c2be02
legendkong <jonathan.kong@sap.com>
2023-10-25 01:31:28 +0800

    edit testing file

--- tests/test_scraping_util.py
+++ tests/test_scraping_util.py
@@ -2,19 +2,19 @@ from src.scraping_util import extract_unique_urls, get_root_url, view_url
 from src.webdriver_util import start_driver
 
 def test_extract_unique_urls():
-    url = "https://learn.deeplearning.ai/"
-    out = extract_unique_urls(url, keyword_list=['course'])
+    url = "https://discovery-center.cloud.sap/serviceCatalog/abap-environment?tab=feature&region=all"
+    out = extract_unique_urls(url, keyword_list=['ABAP Environment'])
     print(out)  # Expected: []
 
 def test_get_root_url():
-    url = 'https://towardsdatascience.com/stable-diffusion-as-an-api-5e381aec1f6'
+    url = 'https://discovery-center.cloud.sap/serviceCatalog/abap-environment?tab=feature&region=all'
     root_url = get_root_url(url)
-    print(root_url)  # Expected: https://towardsdatascience.com
+    print(root_url)  # Expected: 'https://discovery-center.cloud.sap
 
 def test_view_url():
     driver = start_driver()
     # always do this to test. This website needs security clearance
-    url = "https://python.langchain.com/docs/modules/data_connection/document_loaders/integrations/web_base"
+    url = "https://discovery-center.cloud.sap/serviceCatalog/abap-environment?tab=feature&region=all"
     out = view_url(url, driver=driver)
     print(out[:500])
 
