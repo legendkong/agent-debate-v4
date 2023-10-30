d365ac75cf28843adbb55f6637b8a2234833bccf
legendkong <jonathan.kong@sap.com>
2023-10-30 20:04:58 +0800

    modify test file

--- tests/test_scraping_util.py
+++ tests/test_scraping_util.py
@@ -3,18 +3,18 @@ from src.webdriver_util import start_driver
 
 def test_extract_unique_urls():
     url = "https://discovery-center.cloud.sap/serviceCatalog/abap-environment?tab=feature&region=all"
-    out = extract_unique_urls(url, keyword_list=['ABAP Environment'])
+    out = extract_unique_urls(url, keyword_list=[''])
     print(out)  # Expected: []
 
 def test_get_root_url():
     url = 'https://discovery-center.cloud.sap/serviceCatalog/abap-environment?tab=feature&region=all'
     root_url = get_root_url(url)
-    print(root_url)  # Expected: 'https://discovery-center.cloud.sap
+    print(root_url)  # Expected: 'https://discovery-center.cloud.sap.com'
 
 def test_view_url():
     driver = start_driver()
     # always do this to test. This website needs security clearance
-    url = "https://discovery-center.cloud.sap/serviceCatalog/abap-environment?tab=feature&region=all"
+    url = 'https://discovery-center.cloud.sap/serviceCatalog/abap-environment?tab=feature&region=all'
     out = view_url(url, driver=driver)
     print(out[:500])
 
