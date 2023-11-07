e8821ae2def01dd13b8cfdf252ea562de03d4410
legendkong <jonathan.kong@sap.com>
2023-11-07 18:09:58 +0800

    reorg to config folder

---
+++ src/config/config.py
@@ -0,0 +1,35 @@
+# Query format
+# query = 'SAP BTP related information'
+
+# Keywords to be contained in secondary sites.
+# Will add url to list if any one matches. Secondary sites defined as links that are on the urls from Google Search
+# If want to take out all secondary sites, set to empty list ['']
+keyword_list = ['sap', 'blogs', 'btp', 'help' 'cloud', 'hana', 'abap','fiori', 'sapbtp', 'sap btp', 'sap cloud platform', 'sap cloud', 'sap hana', 'sap abap', 'sapui5', 'sap fiori', 'api']
+
+# Unwanted url segments. Any url with these inside usually means proprietary sites, or irrelevant (job) sites
+unwanted_url = ['news', 'linkedin', 'instagram', 'twitter', 'youtube', 'facebook', 'indeed', 'glassdoor', 'monster', 'career', 'job', 'jobs', 'recruit', 'recruitment', 'recruiter', 'hiring', 'apply', 'career', 'people']
+
+# Number of secondary urls to extract each site. GPT will pick the most relevant ones
+max_url_per_site = 3
+
+# Number of google queries to search for. Increasing this increases the breadth of search, but we only have 100 searches a day
+num_google_queries = 1
+
+# Whether to take top 20 search results (default is 10)
+twenty = False
+
+output_format = {
+    "Step Number" : "Summary of the step",
+    "Description" : "A full description of the step",
+    "Source website": "Website of source"
+}
+
+    
+
+curated_format = {
+    "Source": "Source website url",
+    "Steps": "Full description of the steps for the customer to execute in order to solve the problem"
+    }
+
+# Base index. When doing the final table, what will be the reference to merge entries
+base_index = "Source"
---
+++ src/config/dependencies.py
@@ -0,0 +1,22 @@
+import os
+import requests
+import openai
+import tiktoken
+from langchain.text_splitter import TokenTextSplitter
+import json
+from bs4 import BeautifulSoup
+from urllib.parse import urlparse
+import pandas as pd
+import pprint
+import re
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+# No information default
+NO_INFO = "No info"
+
+# Max token length for chatgpt
+MAX_TOKENS = 4000
+
+# max timeout to wait for url opening (let js elements load up in selenium web browser before extracting html)
+timeout = 40
