b997dac3edc74ab16e30e4583178412ec33a0728
legendkong <jonathan.kong@sap.com>
2024-01-16 00:54:47 +0800

    frontend: fix `formatResponse`.ts

--- frontend/lib/formatResponse.ts
+++ frontend/lib/formatResponse.ts
@@ -7,6 +7,19 @@ export function newFormatBTPExpertResponse(text: string): string {
     '<sup><a href="#ref$1">$1</a></sup>'
   )
 
+  // // Format headers that start with "##"
+  // formattedText = formattedText.replace(
+  //   /\n##\s*(.*?)(?=\n|$)/g,
+  //   '<p><strong>$1</strong></p>'
+  // )
+
+  // // Add line breaks for numbered points but exclude complex number patterns
+  // // Adjust this regex to fit your specific needs for numbering
+  // formattedText = formattedText.replace(/(\b\d+\.\s)/g, '<br /><br />$1')
+
+  // // Insert a line break before sentences that end with a colon
+  // formattedText = formattedText.replace(/([^:\n\r]+:)/g, '<br /><br />$1')
+
   // Convert Markdown links to HTML links (displaying only link text)
   // and make them open in a new tab, with underline and teal color
   formattedText = formattedText.replace(
@@ -30,7 +43,12 @@ export function newFormatBTPExpertResponse(text: string): string {
   )
 
   // Add line breaks for numbered points
-  formattedText = formattedText.replace(/(\d+\.)\s*/g, '<br /><br />$1 ')
+  // formattedText = formattedText.replace(/(\d+\.)\s*/g, '<br /><br />$1 ')
+  // Add line breaks for numbered points but exclude number-period-number patterns
+  formattedText = formattedText.replace(/(\d+\.(?!\d))\s*/g, '<br/><br />$1 ')
+
+  // // Insert a line break before sentences that end with a colon
+  // formattedText = text.replace(/([^:\n\r]+:)/g, '<br />$1')
 
   // Add two line breaks before "Reference" and format it in light blue with inline style
   formattedText = formattedText.replace(
@@ -49,9 +67,6 @@ export function newFormatBTPExpertResponse(text: string): string {
 
   // Format italic text (if you have patterns like *text*)
   formattedText = formattedText.replace(/\*(.*?)\*/g, '<em>$1</em>')
-
-  // Additional general formatting rules can be added here
-
   return formattedText
 }
 
